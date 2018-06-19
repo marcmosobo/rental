4de23a70096f7af9718ba8ef7a67459d7cd27de0
amunyua <alexmunyua10@gmail.com>
2018-06-19 10:41:46 +0300

    changes

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -7,6 +7,7 @@ use App\Http\Requests;
 use App\Http\Requests\CreateRoleRequest;
 use App\Http\Requests\UpdateRoleRequest;
 use App\Models\AccessLevel;
+use App\Models\Role;
 use App\Models\RoleRoute;
 use App\Models\Route;
 use App\Repositories\RoleRepository;
@@ -63,10 +64,14 @@ class RoleController extends AppBaseController
      */
     public function store(CreateRoleRequest $request)
     {
+
         $input = $request->all();
         $input['created_by'] = Auth::id();
-        $input["client_id"] = Auth::user()->tenant_id;
-        $role = $this->roleRepository->create($input);
+        $input["client_id"] = Auth::user()->client_id;
+//        print_r($input);die;
+//        $role = Role::create([
+//
+//        ]);
 
         Flash::success('Role saved successfully.');
 
