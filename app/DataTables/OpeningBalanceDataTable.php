fe12c23c58d15bd6339fc7ee4f607b61f8948afa
amunyua <alexmunyua10@gmail.com>
2018-07-14 12:21:51 +0300

    changes

--- app/DataTables/OpeningBalanceDataTable.php
+++ app/DataTables/OpeningBalanceDataTable.php
@@ -2,6 +2,7 @@
 
 namespace App\DataTables;
 
+use App\Http\Controllers\LoggedUserController;
 use App\Models\OpeningBalance;
 use Yajra\DataTables\Services\DataTable;
 use Yajra\DataTables\EloquentDataTable;
@@ -29,7 +30,7 @@ class OpeningBalanceDataTable extends DataTable
      */
     public function query(OpeningBalance $model)
     {
-        return $model->newQuery()->with(['landlord']);
+        return $model->newQuery()->select('opening_balances.*')->with(['landlord']);
     }
 
     /**
@@ -39,21 +40,39 @@ class OpeningBalanceDataTable extends DataTable
      */
     public function html()
     {
-        return $this->builder()
-            ->columns($this->getColumns())
-            ->minifiedAjax()
-            ->addAction(['width' => '80px'])
-            ->parameters([
+        if(LoggedUserController::isAllAccessGranted()){
+            return $this->builder()
+                ->columns($this->getColumns())
+                ->minifiedAjax()
+                ->addAction(['width' => '80px'])
+                ->parameters([
 //                'dom'     => 'Bfrtip',
-                'order'   => [[0, 'desc']],
-                'buttons' => [
-                    'create',
-                    'export',
-                    'print',
-                    'reset',
-                    'reload',
-                ],
-            ]);
+                    'order'   => [[0, 'desc']],
+                    'buttons' => [
+                        'create',
+                        'export',
+                        'print',
+                        'reset',
+                        'reload',
+                    ],
+                ]);
+        }else{
+            return $this->builder()
+                ->columns($this->getColumns())
+                ->minifiedAjax()
+//                ->addAction(['width' => '80px'])
+                ->parameters([
+//                'dom'     => 'Bfrtip',
+                    'order'   => [[0, 'desc']],
+                    'buttons' => [
+                        'create',
+                        'export',
+                        'print',
+                        'reset',
+                        'reload',
+                    ],
+                ]);
+        }
     }
 
     /**
